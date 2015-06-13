3ee14bd1244106e47512cc94df3de21e3dcfd7e1
Javier GM <bifuer@gmail.com>
2015-06-13 18:43:01 +0200

    New file cache system for templates sources

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var fs = require('fs');
 var crier = require('crier').addGroup('templated');
 var i18n = require('textualization');
 
-var cache = {};
 var engines = {};
 
 module.exports = global.modulesCache['templated'] = render;
@@ -37,39 +36,34 @@ function render(path,locals,reload,callback){
 }
 
 function load(path,reload,callback){
-  var ext = path.replace(/^.*\./,'');
-  var engine = engines[ext] || false;
-  if(cache[path] && !reload){
-    if(callback){
-      callback(undefined,cache[path]);
-    }
-  } else {
-    if(engine){
-      fs.readFile(path,{encoding:'utf8'},function(error,source){
-        try {
-          if(error){
-            throw error;
-          }
-          cache[path]=engine(source,path);
-          crier.info('loaded',{path:path});
-          if(callback){
-            callback(undefined,cache[path]);
-          }
-        } catch (error){
-          crier.error('compileError',{path:path,error:error});
-          if(callback){
-            callback(error);
-          }
-        }
-      });
-    } else {
+  templateCache.get(path,reload,function(error,template){
+    if(error){
+      if(callback){
+        callback(error);
+      }
+    } if(!template.engine){
       var error = new Error("Unknow engine");
       crier.alert('unknowEngine',{path:path,error:error});
       if(callback){
         callback(error);
       }
+    } else {
+      try {
+        if(!template.compiled){
+          template.compiled = template.engine(template.data,path);
+          crier.info('loaded',{path:path});
+        }
+        if(callback){
+          callback(undefined,template.compiled);
+        }
+      } catch(error){
+        crier.error('compileError',{path:path,error:error});
+        if(callback){
+          callback(error);
+        }
+      }
     }
-  }
+  });
 }
 
 function addEngine(ext,compiler){
@@ -83,3 +77,47 @@ function removeEngine(ext){
 function getEngines(){
   return Object.keys(engines);
 }
+
+var templateCache = {
+  items:{},
+  get : function(id,fresh,callback){
+    var item = templateCache.items[id];
+    if(typeof item !== 'undefined'){
+      if(item.timeStamp<Date.now()-30*1000 || !fresh){
+        callback(null,item);
+      } else {
+        templateCache.file(id,callback,item);
+      }
+    } else {
+      templateCache.file(id,callback);
+    }
+  },
+  file: function(id,callback,update){
+    var item = update?update:{};
+    fs.stat(id, function(error,stats){
+      if(error){
+        callback(error);
+      } else {
+        item.timeStamp = Date.now();
+        if(!update || stats.mtime>item.stats.mtime){
+          fs.readFile(id, function(error,data){
+            if(error){
+              callback(error);
+            } else {
+              templateCache.items[id] = item;
+              item.stats = stats;
+              item.ext = id.replace(/^.*\./,'');
+              item.engine = engines[item.ext] || false;
+              delete item.data;
+              delete item.compiled;
+              item.data = data;
+              callback(null,item);
+            }
+          });
+        } else {
+          callback(null,item);
+        }
+      }
+    });
+  }
+};
